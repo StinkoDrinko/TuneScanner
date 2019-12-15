8271fc5c8a0b2b0f8fa579f40230eb47557994a1
theCheeese <msyrov96@gmail.com>
2019-12-14 20:13:24 -0500

    implement soundplayer

---
+++ app/src/main/java/com/anton/tunescanner/SoundPlayer.java
@@ -0,0 +1,44 @@
+package com.anton.tunescanner;
+
+import android.media.MediaPlayer;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SoundPlayer {
+
+    public SoundPlayer(File soundFile) throws IllegalArgumentException {
+        if(!checkFile(soundFile))
+            throw new IllegalArgumentException("wrong file type: must be 3gp, wav, mp3, or other sound file");
+        this.soundFile = soundFile;
+        player = new MediaPlayer();
+    }
+
+    private boolean checkFile(File file) {
+        boolean isValidSoundFile = false;
+
+        String filename = file.getName();
+        String[] filenameTokens = filename.split("\\.");
+
+        for(String token : filenameTokens) {
+            if(token.matches("3gp|mp3|wav")) {
+                isValidSoundFile = true;
+                break;
+            }
+        }
+        return isValidSoundFile;
+    }
+
+    public void startPlayback() throws IOException {
+        player.setDataSource(soundFile.getAbsolutePath());
+        player.prepare();
+        player.start();
+    }
+
+    public void stopPlayback() {
+        player.stop();
+    }
+
+    private MediaPlayer player;
+    private File soundFile;
+}
